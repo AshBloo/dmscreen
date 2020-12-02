823464c86c09a5576b369e4e87f3574a10d569a0
AshBloo <ashjones1993@outlook.com>
2020-12-02 15:12:15 +0000

    Condensed the two dictionaries handling the button commands and labels into one dictionary handling both

--- simpleDice.py
+++ simpleDice.py
@@ -12,10 +12,9 @@ def d20():
 
 window=tk.Tk()
 window.rowconfigure(0, weight=1, minsize=50)
-btn_labels={0: "Flip a coin", 1: "Roll a d6", 2: "Roll a d20"}
-btn_commands={0: coinflip, 1: d6, 2: d20}
+btn_config={"Flip a coin": coinflip, "Roll a d6": d6, "Roll a d20": d20}
 
-for i in btn_labels:
+for i, label in enumerate(btn_config):
     window.columnconfigure(i, weight=1, minsize=20)
     frame=tk.Frame(
         master=window,
@@ -23,16 +22,16 @@ for i in btn_labels:
         borderwidth=1
     )
     frame.grid(row=0, column=i, padx=5)
-    button=tk.Button(master=frame, text=f"{btn_labels[i]}", command=btn_commands[i])
+    button=tk.Button(master=frame, text=label, command=btn_config.get(label))
     button.pack()
 
-window.columnconfigure(len(btn_labels)+1, weight=2, minsize=20)
+window.columnconfigure(len(btn_config)+1, weight=2, minsize=20)
 frame=tk.Frame(
     master=window,
     relief=tk.FLAT,
     borderwidth=1
 )
-frame.grid(row=0,column=len(btn_labels)+1,padx=5)
+frame.grid(row=0,column=len(btn_config)+1,padx=5)
 lbl_result=tk.Label(master=frame, text="0")
 lbl_result.pack()
 
